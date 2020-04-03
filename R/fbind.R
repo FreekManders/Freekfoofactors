c3cf436a60dcb35d07a475a977213ac6c4235d5f
FreekManders <freek.manders@gmail.com>
2020-04-03 12:26:34 +0200

    Added documentation for fbind.

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,16 @@
+#' Function to concatenate two factors.
+#'
+#'Bind two factors together.
+#' @param a factor
+#' @param b factor
+#'
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+#'
 fbind <- function(a, b) {
     factor(c(as.character(a), as.character(b)))
 }
